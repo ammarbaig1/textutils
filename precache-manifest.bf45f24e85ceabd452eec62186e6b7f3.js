f6186a88976c34d4065ac14c8d14b0f0661e2948
ammarbaig1 <128991949+ammarbaig1@users.noreply.github.com>
2023-03-27 01:53:28 -0700

    Add files via upload

---
+++ precache-manifest.bf45f24e85ceabd452eec62186e6b7f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3007bd64df388d39e638",
+    "url": "/textutils/static/css/main.303d705a.chunk.css"
+  },
+  {
+    "revision": "3007bd64df388d39e638",
+    "url": "/textutils/static/js/main.3007bd64.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "91449b700f0ab8a2a413",
+    "url": "/textutils/static/js/runtime~main.91449b70.js"
+  },
+  {
+    "revision": "b929e69ccf53478dc571ab33dc5ea8be",
+    "url": "/textutils/index.html"
+  }
+];
